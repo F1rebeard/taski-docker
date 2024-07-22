2344f7eef69dffe48c191995c9d0009401891f7d
Александр Саратов <alxsrtv@gmail.com>
2024-07-22 16:22:47 +0300

    added Postreges and api tests

---
+++ backend/api/tests.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from http import HTTPStatus
+from symtable import Class
+
+from api import models
+from django.test import Client, TestCase
+
+
+class TaskiAPITestCase(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности списка задач."""
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """Проверка создания задачи."""
+        data = {
+            'title': 'Test',
+            'description': 'Test'
+        }
+        response = self.guest_client.get('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
